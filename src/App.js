cf1904ab0068142672b0e6b01eeef747603942d9
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-11-23 20:31:15 -0600

    Add output section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return null;
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="currnet-operand"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
